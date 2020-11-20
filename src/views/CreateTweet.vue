1cbd20cb14f7ba77bc670c3ea6d8a1e03f018bde
WatanabeKeitaa <keita520@gmail.com>
2020-11-21 02:50:35 +0900

    [add]筋トレ投稿、料理投稿画面の張りぼて作成完了

--- src/views/CreateTweet.vue
+++ src/views/CreateTweet.vue
@@ -5,16 +5,60 @@
       <!-- <input v-model="name" placeholder="item name" />
       <input v-model="description" placeholder="item description" /> -->
       <!-- <button :disabled="loading" @click="mutate">Create Todo</button> -->
-          <v-form>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="3">
-                  <v-text-field
-                    label="content"
-                    placeholder="内容"
-                    v-model="content"
-                  ></v-text-field>
-                </v-col>
+      <v-form>
+
+        <v-container>
+
+          <v-row>
+
+            <!-- 戻るボタンです -->
+            <v-col cols="12" sm="6" md="3" justify="left">
+              <v-btn
+                class="ma-2"
+                color="primary"
+                dark
+                @click="back"
+              >
+              <v-icon
+                dark
+                left
+              >
+                mdi-arrow-left
+              </v-icon>Back</v-btn>
+            </v-col>
+
+            <!-- 下書きボタンです、中身はまだない -->
+            <v-col cols="12" sm="6" md="3" justify="right">
+              <v-btn
+                class="ma-2"
+                color="primary"
+                dark
+              >
+                下書き
+              </v-btn>
+
+              <!-- 投稿ボタン -->
+              <v-btn
+                class="ma-2"
+                color="primary"
+                dark
+              >
+                投稿
+              </v-btn>
+            </v-col>
+
+          </v-row>
+
+          <v-row>
+
+            <!-- 投稿記述場所 -->
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field
+                label="content"
+                placeholder="内容"
+                v-model="content"
+              ></v-text-field>
+            </v-col>
                 <!-- <v-col>
                   <v-text-field
                     label="description"
@@ -22,15 +66,10 @@
                     v-model="description"
                   ></v-text-field>
                 </v-col> -->
-                <v-col>
-                  <v-btn @click="createTweet" large color="primary">投稿</v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-
+          </v-row>
+        </v-container>
+      </v-form>
     </template>
-   
   </div>
 </template>
 
@@ -60,6 +99,11 @@ export default {
   computed: {
   },
   methods: {
+    // 戻るボタンメソッドです
+    back: function(){
+      this.$router.push('/');
+      // historyできてなくね？
+    },
     async createTweet(){
       const tweet = await API.graphql(
         //[fix]あとでクエリー書き直す?
