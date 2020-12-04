92ef6fcb8d747b94295aee3cba7d198a0441de3d
WatanabeKeitaa <keita520@gmail.com>
2020-12-04 18:54:22 +0900

    [update]プロフィール編集画面の中身作成

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -8,19 +8,36 @@
     <!-- <div>
       プロフィールへんしうがめｎ
     </div> -->
-      <v-btn
-        class="ma-2"
-        color="primary"
-        dark
-        @click="back"
-      >
-        <v-icon
+    <v-row>
+      <v-col>
+        <v-btn
+          class="ma-2"
+          color="primary"
           dark
-          left
+          @click="back"
         >
-          mdi-arrow-left
-        </v-icon>Back
-      </v-btn>
+          <v-icon
+            dark
+            left
+          >
+            mdi-arrow-left
+          </v-icon>Back
+        </v-btn>
+      </v-col>
+
+      <!-- 保存ボタン、名前が変更されたら出てくる -->
+      <transition name="button">
+        <v-btn
+          v-show="buttonActive"
+          class="ma-2"
+          color="primary"
+          dark
+          @click="save"
+        >
+          保存
+        </v-btn>
+      </transition>
+    </v-row>
       
       <!-- アカウントアイコン -->
       <v-row justify="center">
@@ -32,53 +49,54 @@
       <!-- 写真変更ボタン -->
       <v-row justify="center">
         <v-btn
-            class="ma-2"
-            color="primary"
-            dark
-          >
-          <v-icon
-            dark
-            left
-          >
-            mdi-camera
-          </v-icon>
-            写真を変更
-          </v-btn>
+          class="ma-2"
+          color="primary"
+          dark
+        >
+        <v-icon
+          dark
+          left
+        >
+          mdi-camera
+        </v-icon>
+          写真を変更
+        </v-btn>
       </v-row>
 
-        <!-- 横線です -->
-        <!-- <v-divider></v-divider> -->
+      <!-- 横線です -->
+      <!-- <v-divider></v-divider> -->
       <v-row justify="center">
         <v-col cols="5">
           <v-text-field
             v-model="name"
-            value=name
             label="名前"
             clearable
           ></v-text-field>
-          <!-- [fix]自己紹介文入力できるようになったらvalueで入れる -->
+          <!-- [fix]自己紹介文入力できるようになったらv-modelで変更できるようにする -->
           <v-textarea
             label="自己紹介"
             clearable
           ></v-textarea>
         </v-col>
       </v-row>
+      <!-- {{name}}
+      {{myid}} -->
 
+      <!-- いい感じのデザインに変える -->
+      <v-row justify="center">
+      <v-dialog
+        v-model="dialog"
+        max-width="290"
+      >
+        <v-card>
+          <v-card-title class="headline">保存完了</v-card-title>
+          <v-card-text>
+            プロフィールを変更しました。
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </v-row>
 
-      <transition name="button">
-        <v-btn
-          v-show="buttonActive"
-          @click="scrollTop"
-          fixed
-          color="primary"
-          dark
-          bottom
-          right
-          fab
-        >
-        <v-icon>mdi-chevron-up</v-icon>
-        </v-btn>
-      </transition>
     </v-container>
   </div>
   <!-- tweetカードらへんをコンポーネントにする。一番上に飛ぶボタンつくる -->
@@ -87,69 +105,76 @@
 <script>
 import Navigation from '@/components/Navigation.vue';
 // import NewTodo from '@/components/NewTodo.vue';
-
-const updateName = `
-mutation UpdateUser {
-  updateUser(
-    input: $id
-    name: "わたなべ"
-    }) 
-    {
-    id
-    name
-  }
-}
-`
+import { API, graphqlOperation } from 'aws-amplify'
+import * as gqlMutations from '../graphql/mutations'
 
 
   export default {
     data() {
       return{
-        // 上に行くボタン用、ここの画面ではスクロールがないので今はtrueにしてます
-        buttonActive: true,
-        scroll: 0,
+        // v-text-field（名前のとこ）の中身
         name: null,
+        // 自分のID入れる用
+        myid: null,
+        // ダイアログ表示用
+        dialog: false,
+        // 名前に変更があったかどうかの比較用
+        name2: null,
+        // 保存ボタンの表示非表示用
+        buttonActive: false,
       }
     },
-    // props:['followees', 'follows'],
     components: {
       Navigation,
+    },
+    // watchでnameを監視する
+    watch: {
+      name: function() {
+        // 変更前の名前name2と監視中の名前nameが正しかった（変更がなかった）ら保存ボタン非表示
+        if (this.name == this.name2) {
+          this.buttonActive = false
+        // 変更があったら保存ボタン表示
+        } else {
+          this.buttonActive = true
+        }
+      }
     },
     computed: {
     },
     methods: {
-    scrollTop: function(){
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      })
-    },
-    // buttonActiveにtrueとfalse渡して表示非表示してる、上行くボタンのv-show="buttonActiveてとこのやつ
-    scrollWindow() {
-      const top = 100 // ボタンを表示させたい位置
-      this.scroll = window.scrollY
-      if (top <= this.scroll) {
-        this.buttonActive = true
-      } else {
-        this.buttonActive = false
-      }
-    },
     back: function(){
       this.$router.push('/Profile');
       // historyできてなくね？
     },
+    // 保存ボタン
+    async save() {
+      const updateName = await API.graphql(
+        graphqlOperation(gqlMutations.updateUser,{
+          input: {
+            id: this.myid,
+            name: this.name,
+          }
+        })
+      )
+      console.log("名前を変更しました" + updateName.data.updateUser)
+      // ここもthenとかerrorで変更成功、変更失敗とか分けた方がいい？
+      this.dialog = true
+    },
   },
   mounted : async function(){
-    // 上行くボタン
-    window.addEventListener('scroll', this.scrollWindow)
+    // 自分の情報取得
     const user= this.$store.getters.getUserGraphql
+    // 現在の自分の名前
     this.name = user.items[0].name
+    this.name2 = user.items[0].name
+    // 自分のID
+    this.myid = user.items[0].id
   }
-  }
+}
 </script>
 
 <style scoped>
-/* 上に行くボタン */
+/* 上に行くボタンのアニメーション？を保存ボタンに使ってる */
 .button-enter-active,
 .button-leave-active {
   transition: opacity 0.5s;
