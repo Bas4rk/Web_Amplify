ed31306b94308441881f0ce6271bb0d07682ced1
irgroup5 <x18g008@chiba-fjb.ac.jp>
2020-12-14 11:43:29 +0900

    [updatre]お困りの方はこちら(よくある質問ページ)、利用規約ページ作成に入りました

--- src/views/setting/SettingHelp.vue
+++ src/views/setting/SettingHelp.vue
@@ -11,7 +11,37 @@
       <v-row justify="center">
         <v-col cols="5">
           <v-list>
-            
+            <v-expansion-panels>
+              <v-expansion-panel
+                v-for="(item,i) in 1"
+                :key="i"
+              >
+                <v-expansion-panel-header>
+                  スポーツやトレーニング経験がないのですが、続けられるでしょうか？
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  トレー二ングが初めてでも大丈夫です
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+
+            <v-expansion-panels>
+              <v-expansion-panel
+                v-for="(item,i) in 1"
+                :key="i"
+              >
+                <v-expansion-panel-header>
+                  トレーニングが飽きやすい私に向いていますか？
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  OKです
+                  そのような方をお待ちしておりました
+                  全くの初心者でも本アプリはお使いいただけます
+                  飽きやすい方にも使えるようSNSを搭載しております
+                  存分にお使いください
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </v-list>
         </v-col>
       </v-row>
