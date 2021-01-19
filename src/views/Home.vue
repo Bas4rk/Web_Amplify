fb02942359d2a524016ebff06decfc624d41c902
huzi <x18g032@chiba-fjb.ac.jp>
2021-01-19 11:30:50 +0900

    dialog未完成2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,7 @@
           dark
           small
           :color="item.color"
-          :to="item.to"
+          @click="onClickBtn(item)"
         >
           <v-icon>{{item.icon}}</v-icon>
         </v-btn>
@@ -90,28 +90,12 @@
     <!-- ◆◆◆ツイートのダイアログ開始◆◆◆ -->
      <v-dialog
         v-model="dialog"
+        v-if="currentItem"
         max-width="290"
       >
         <v-form>
           <v-container>
             <v-row>
-
-              <!-- 戻るボタンです -->
-              <v-col cols="12" sm="6" md="3" justify="left">
-                <v-btn
-                  class="ma-2"
-                  color="primary"
-                  dark
-                  @click="back"
-                >
-                <v-icon
-                  dark
-                  left
-                >
-                  mdi-arrow-left
-                </v-icon>Back</v-btn>
-              </v-col>
-
               <!-- 下書きボタンです、中身はまだない -->
               <v-col cols="12" sm="6" md="3" justify="right">
                 <v-btn
@@ -183,7 +167,7 @@ export default {
       left: false,
       transition: 'slide-y-transition',
       items: [
-        { color: 'green', dialog: true, icon: 'mdi-comment' },
+        { color: 'green', to: 'dialog = true', icon: 'mdi-comment' },
         { color: 'red', to: '/createCooking', icon: 'mdi-silverware-fork-knife' },
         { color: 'indigo', to: '/createTraining', icon: 'mdi-dumbbell' },
       ],
@@ -194,6 +178,7 @@ export default {
       //直前のパス
       prevRoute: 'tweet',
       //ツイート管理用
+      currentItem: null,
       content: '',
       dialog: false,
     }
@@ -224,6 +209,25 @@ export default {
         this.buttonActive = false
       }
     },
+    async createTweet(){
+    const tweet = await API.graphql(
+      //[fix]あとでクエリー書き直す?
+      graphqlOperation(gqlMutations.createTweet, {
+        input: {
+          content: this.content,
+          userId: this.$store.getters.getUserId
+        }
+      })
+    )
+    console.log(tweet.data.createTweet);
+    // createTweetにそのまま書いてるけど、thenとかerrorで投稿成功、投稿失敗とか分けた方がいいと思った。
+    this.dialog = false;
+    this.content = '';
+  },
+  onClickBtn(item) {
+    this.currentItem = item
+    this.dialog = true
+  }
   },
   mounted : async function(){
     // 上行くボタン
@@ -273,21 +277,6 @@ export default {
     next(vm => {
       vm.prevRoute = from.name;
     });
-  },
-  async createTweet(){
-    const tweet = await API.graphql(
-      //[fix]あとでクエリー書き直す?
-      graphqlOperation(gqlMutations.createTweet, {
-        input: {
-          content: this.content,
-          userId: this.$store.getters.getUserId
-        }
-      })
-    )
-    console.log(tweet.data.createTweet);
-    // createTweetにそのまま書いてるけど、thenとかerrorで投稿成功、投稿失敗とか分けた方がいいと思った。
-    this.dialog = false;
-    this.content = '';
   }
 }
 </script>
