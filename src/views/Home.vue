74ab1b1edc6b543f377cc403169e32eda38468eb
huzi <x18g032@chiba-fjb.ac.jp>
2021-01-20 11:50:04 +0900

    TweetDialog未完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -85,14 +85,14 @@
       <v-icon>mdi-chevron-up</v-icon>
       </v-btn>
     </transition>
-
-
+    
     <!-- ◆◆◆ツイートのダイアログ開始◆◆◆ -->
-     <v-dialog
-        v-model="dialog"
-        v-if="currentItem"
-        max-width="290"
-      >
+    <v-dialog
+      v-model="dialog"
+      v-if="currentItem"
+      max-width="1000"
+    >
+      <v-card v-show="currentItem==0">  <!-- TweetCreate -->
         <v-form>
           <v-container>
             <v-row>
@@ -131,7 +131,368 @@
             </v-row>
           </v-container>
         </v-form>
-      </v-dialog>
+      </v-card>
+      <v-card v-show="currentItem==1">  <!-- CookingCreate -->
+        <div class="CreateCooking">
+          <Navigation></Navigation>
+
+          <!-- v-layout justify-centerで中央に持ってくる -->
+          <v-layout justify-center>
+
+            <!-- class="title"で文字の大きさを変えてます、ここ"display-1"でもいいかも -->
+            <div class="title">料理投稿画面</div>
+
+          </v-layout>
+
+          <!-- 横線です -->
+          <v-divider></v-divider>
+
+          <v-container>
+
+            <v-row>
+
+              <!-- 戻るボタンです -->
+              <v-col cols="12" sm="6" md="3" justify="left">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  @click="back"
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-arrow-left
+                </v-icon>Back</v-btn>
+              </v-col>
+
+              <!-- これ下書きボタンの位置調整用に書いた -->
+              <v-col cols="12" sm="6" md="3" justify="center">
+              </v-col>
+
+              <!-- 下書きボタンです、中身はまだない -->
+              <v-col cols="12" sm="6" md="3" justify="right">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                >
+                  下書き
+                </v-btn>
+
+                <!-- 投稿ボタン -->
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  @click="createCooking"
+                >
+                  投稿
+                </v-btn>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-img
+                v-if="uploadImageUrl"
+                :src="uploadImageUrl"
+                max-width="600"
+                max-height="600">
+              </v-img>
+            </v-row>
+
+            <v-row>
+              <v-col cols="5">
+              <v-file-input
+                accept="image/*"
+                label="料理の写真を載せる"
+                @change="onImgPicked"
+              ></v-file-input>
+              </v-col>
+            </v-row>
+            <!-- {{this.uploadImageUrl}}
+            {{this.image}} -->
+
+            <v-row>
+
+              <!-- タイトル入力 -->
+              <v-col cols="12" sm="6" md="3">
+                <v-text-field
+                  placeholder="タイトル"
+                  v-model="title"
+                >
+                </v-text-field>
+              </v-col>
+
+              <!-- カロリー入力ボタンです、中身はまだない、どうやって入力させる？ -->
+              <!-- ここ一旦ボタンやめて数値入力させるようにしました -->
+              <v-col cols="12" sm="6" md="3">
+                <!-- <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  style="text-transform: none"
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-food
+                </v-icon>
+                  ~kcal
+                </v-btn> -->
+                <v-text-field
+                  placeholder="カロリー"
+                  v-model="calorie"
+                >
+                </v-text-field>
+              </v-col>
+
+              <!-- 栄養素入力です -->
+              <!-- <v-col cols="12" sm="6" md="3">
+                <v-text-field
+                  placeholder="たんぱく質"
+                  suffix="g"
+                >
+                </v-text-field>
+                <v-text-field
+                  placeholder="脂質"
+                  suffix="g"
+                >
+                </v-text-field>
+                <v-text-field
+                  placeholder="炭水化物"
+                  suffix="g"
+                >
+                </v-text-field>
+                <v-text-field
+                  placeholder="食物繊維総量"
+                  suffix="g"
+                >
+                </v-text-field>
+              </v-col> -->
+
+            </v-row>
+
+            <!-- 作り方書くところ -->
+            <v-row>
+
+              <!-- clearableいらない？ -->
+              <v-col cols="12" sm="6" md="3">
+                <v-textarea
+                  label="料理の作り方！"
+                  clearable
+                  clear-icon="mdi-close-circle"
+                  v-model="content"
+                >
+                </v-textarea>
+              </v-col>
+
+              <!-- 写真投稿ボタンです、中身はまだない -->
+              <v-col cols="12" sm="6" md="3" justify="left">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-camera
+                </v-icon>
+                  料理の写真を載せる
+                </v-btn>
+              </v-col>
+
+            </v-row>
+
+            <v-row>
+
+              <!-- 作り方追加 -->
+              <v-col cols="12" sm="6" md="3">
+                <v-btn
+                  block
+                  class="ma-2"
+                  color="primary"
+                  dark
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-plus
+                </v-icon>
+                  作り方を追加
+                </v-btn>
+              </v-col>
+
+            </v-row>
+          </v-container>
+        </div>
+      </v-card>
+      <v-card v-show="currentItem==2">  <!-- TrainingCreate -->
+        <div class="CreateTraining">
+          <Navigation></Navigation>
+
+          <!-- v-layout justify-centerで中央に持ってくる -->
+          <v-layout justify-center>
+
+            <!-- class="title"で文字の大きさを変えてます、ここ"display-1"でもいいかも -->
+            <div class="title">筋トレ投稿画面</div>
+
+          </v-layout>
+
+          <!-- 横線です -->
+          <v-divider></v-divider>
+
+          <v-container>
+
+            <v-row>
+
+              <!-- 戻るボタンです -->
+              <v-col cols="12" sm="6" md="3" justify="left">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  @click="back"
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-arrow-left
+                </v-icon>Back</v-btn>
+              </v-col>
+
+              <!-- これ下書きボタンの位置調整用に書いた -->
+              <v-col cols="12" sm="6" md="3" justify="center">
+              </v-col>
+
+              <!-- 下書きボタンです、中身はまだない -->
+              <v-col cols="12" sm="6" md="3" justify="right">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                >
+                  下書き
+                </v-btn>
+
+                <!-- 投稿ボタン -->
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  @click="createTraining"
+                >
+                  投稿
+                </v-btn>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-img
+                v-if="uploadImageUrl"
+                :src="uploadImageUrl"
+                max-width="600"
+                max-height="600">
+              </v-img>
+            </v-row>
+
+            <v-row>
+              <v-col cols="5">
+              <v-file-input
+                accept="image/*"
+                label="筋トレの写真を載せる"
+                @change="onImgPicked"
+              ></v-file-input>
+              </v-col>
+            </v-row>
+            <!-- {{this.uploadImageUrl}}
+            {{this.image}} -->
+
+            <v-row>
+              <!-- タイトル入力 -->
+              <v-col cols="12" sm="6" md="3">
+                <v-text-field
+                  placeholder="タイトル"
+                  v-model="title"
+                >
+                </v-text-field>
+              </v-col>
+
+              <!--タグ入力です、Prottにあったから作った、でもDynamoDBに？タグの項目ない -->
+              <v-col cols="12" sm="6" md="3">
+                <v-text-field
+                  placeholder="#タグ（必須でない）"
+                >
+                </v-text-field>
+              </v-col>
+
+            </v-row>
+
+            <!-- 作り方書くところ -->
+            <v-row>
+
+              <!-- clearableいらない？ -->
+              <v-col cols="12" sm="6" md="3">
+                <v-textarea
+                  label="メニュー"
+                  placeholder="1.これを３０秒行う。"
+                  clearable
+                  clear-icon="mdi-close-circle"
+                  v-model="content"
+                >
+                </v-textarea>
+              </v-col>
+
+              <!-- 写真投稿ボタンです、中身はまだない -->
+              <v-col cols="12" sm="6" md="3" justify="left">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-camera
+                </v-icon>
+                  写真を載せる
+                </v-btn>
+              </v-col>
+
+            </v-row>
+
+            <v-row>
+
+              <!-- メニュー追加 -->
+              <v-col cols="12" sm="6" md="3">
+                <v-btn
+                  block
+                  class="ma-2"
+                  color="primary"
+                  dark
+                >
+                <v-icon
+                  dark
+                  left
+                >
+                  mdi-plus
+                </v-icon>
+                メニューを追加
+                </v-btn>
+              </v-col>
+
+            </v-row>
+          </v-container>
+        </div>
+      </v-card>
+    </v-dialog>
       <!-- ◆◆◆ツイートのダイアログ終了◆◆◆ -->
   </div>
 </template>
@@ -194,9 +555,9 @@ export default {
       left: false,
       transition: 'slide-y-transition',
       items: [
-        { color: 'green', to: 'dialog = true', icon: 'mdi-comment' },
-        { color: 'red', to: '/createCooking', icon: 'mdi-silverware-fork-knife' },
-        { color: 'indigo', to: '/createTraining', icon: 'mdi-dumbbell' },
+        { color: 'green', to: '0', icon: 'mdi-comment' },
+        { color: 'red', to: '1', icon: 'mdi-silverware-fork-knife' },
+        { color: 'indigo', to: '2', icon: 'mdi-dumbbell' },
       ],
       //ポストリスト
       tweetPosts: null,
@@ -254,7 +615,7 @@ export default {
     this.content = '';
   },
   onClickBtn(item) {
-    this.currentItem = item
+    this.currentItem = item.to
     this.dialog = true
   },
   subscribe(){
